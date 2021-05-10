1ffc97c00518ebfa7d6827f73cf9591bc5c3d48f
Rewa Patil <>
2021-05-10 13:07:37 +0000

    correct js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // connect "Positive or negative" button to "comparison" function
 document.getElementById('click').addEventListener('click', comparison)
-let entnum = 0
 
-function comparison() {
+function comparison () {
   // get user input and convert to integer
-  const entnum = parseInt(document.getElementById('entnum').value)
+  let entnum = parseInt(document.getElementById('entnum').value)
 
   // check if the usersInput is less than 0
   if (entnum < 0) {
     document.getElementById('answer').innerHTML = 'Negative'
   } else {
-    document.getElementById('answer').innerHTML = "Positive"
+    document.getElementById('answer').innerHTML = 'Positive'
   }
 }
