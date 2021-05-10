c9338e20f607c08c0eb18c80125c6a0eb3f7651d
Rewa Patil <>
2021-05-10 00:49:22 +0000

    indentation

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//connect "Positive or negative" button to "comparison" function
-document.getElementById('button').addEventListener('click', comparison)
-let userInput = 0
+// connect "Positive or negative" button to "comparison" function
+document.getElementById('click').addEventListener('click', comparison)
+let entnum = 0
 
 function comparison () {
-    // get user input and convert to integer
-  userInput = document.getElementById('input').value
-  userInput = parseInt(userInput)
+  // get user input and convert to integer
+  const entnum = parseInt(document.getElementById('entnum').value)
+  
 
   // check if the usersInput is less than 0
   if (userInput < 0) {
-     document.getElementById('answer').innerHTML = "Negative"
+    document.getElementById('answer').innerHTML = 'Negative'
+  } if (userInput > 0) {
+    document.getElementById('answer').innerHTMl = "Positive"
   }
-
-}
+ 
