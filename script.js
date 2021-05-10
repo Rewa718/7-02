42112fcdb7790a2912d162c53035bdf08f4a5460
Rewa Patil <>
2021-05-10 14:43:01 +0000

    use const

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('click').addEventListener('click', comparison)
 
 function comparison () {
   // get user input and convert to integer
-  let entnum = parseInt(document.getElementById('entnum').value)
+  const entnum = parseInt(document.getElementById('entnum').value)
 
   // check if the usersInput is less than 0
   if (entnum < 0) {
