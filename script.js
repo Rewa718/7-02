b1c474de15003b3bacd68e07e25fd3b8c4056f1c
Rewa Patil <>
2021-05-10 13:03:06 +0000

    corrections

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 document.getElementById('click').addEventListener('click', comparison)
 let entnum = 0
 
-function comparison () {
+function comparison() {
   // get user input and convert to integer
   const entnum = parseInt(document.getElementById('entnum').value)
-  
 
   // check if the usersInput is less than 0
-  if (userInput < 0) {
+  if (entnum < 0) {
     document.getElementById('answer').innerHTML = 'Negative'
-  } if (userInput > 0) {
-    document.getElementById('answer').innerHTMl = "Positive"
+  } else {
+    document.getElementById('answer').innerHTML = "Positive"
   }
+}
